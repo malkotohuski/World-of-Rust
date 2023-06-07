5c9f263d9bbfb8b92a00563223075c05ab59687e
Daniel Dimitrov <malkothuski@gmail.com>
2023-06-07 15:30:13 +0300

    some css changes

--- src/componets/ForgotPassword/ForgotPassword.js
+++ src/componets/ForgotPassword/ForgotPassword.js
@@ -6,6 +6,8 @@ const ForgotPassword = () => {
   const [email, setEmail] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
+  const [resetSuccess, setResetSuccess] = useState(false);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -21,34 +23,55 @@ const ForgotPassword = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Perform password reset logic here
-    // Validate email, new password, and confirm password
-    // If valid, update the password and redirect to the success page
-    // Otherwise, display appropriate error messages
+
+    if (newPassword !== confirmPassword) {
+      setConfirmPasswordError('New password and confirm password do not match.');
+    } else {
+      setConfirmPasswordError(''); // Clear the error message if passwords match
+
+      // Perform password reset logic here
+      // Validate email, new password, and confirm password
+      // If valid, update the password and redirect to the success page
+      // Otherwise, display appropriate error messages
+
+      // Assuming password reset is successful:
+      setResetSuccess(true);
+      setEmail('');
+      setNewPassword('');
+      setConfirmPassword('');
+    }
   };
 
   return (
     <div className="forgot-password-container">
-      <h1>Forgot Password</h1>
-      <form onSubmit={handleSubmit} className="forgot-password-form">
-        <div className="form-group">
-          <label>Email:</label>
-          <input type="email" value={email} onChange={handleEmailChange} />
+      {resetSuccess ? (
+        <div>
+          <p className="success-message">Password reset. Please check your email.</p>
+          <div className="centered-button">
+            <Link to="/login" className="login-btn">Login</Link>
+          </div>
         </div>
-        <div className="form-group">
-          <label>New Password:</label>
-          <input type="password" value={newPassword} onChange={handleNewPasswordChange} />
+      ) : (
+        <div>
+          <h1>Forgot Password</h1>
+          <form onSubmit={handleSubmit} className="forgot-password-form">
+            <div className="form-group">
+              <label>Email:</label>
+              <input type="email" value={email} onChange={handleEmailChange} />
+            </div>
+            <div className="form-group">
+              <label>New Password:</label>
+              <input type="password" value={newPassword} onChange={handleNewPasswordChange} />
+            </div>
+            <div className="form-group">
+              <label>Confirm Password:</label>
+              <input type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} />
+              {confirmPasswordError && <p className="error-message">{confirmPasswordError}</p>}
+            </div>
+            <button type="submit" className="reset-password-button">Reset Password</button>
+          </form>
         </div>
-        <div className="form-group">
-          <label>Confirm Password:</label>
-          <input type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} />
-        </div>
-        <button type="submit" className="reset-password-button">Reset Password</button>
-      </form>
-      <h3 className='text-white'>Remember your password?</h3>
-      <p>
-      <Link className="text-login" to="/login">Log in</Link>
-      </p> 
+      )}
     </div>
   );
 };
