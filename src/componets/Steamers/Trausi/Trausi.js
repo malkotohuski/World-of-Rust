8fdffc9ac3daa57fd879b8eb7dcade250939b3d2
Daniel Dimitrov <malkothuski@gmail.com>
2023-09-27 15:55:25 +0300

    game time added

--- src/componets/Steamers/Trausi/Trausi.js
+++ src/componets/Steamers/Trausi/Trausi.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './Trausi.css';
-import trausiImage from '../../../images/trausi-image.png'; // Replace with the actual path to your Trausi image file
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "./Trausi.css";
+import trausiImage from "../../../images/trausi-image.png"; // Replace with the actual path to your Trausi image file
+import { useNavigate } from "react-router-dom";
 
 function Trausi() {
     const navigate = useNavigate();
@@ -11,16 +11,16 @@ function Trausi() {
         // You can implement the functionality to add a comment
         // based on your requirements or use case.
 
-        navigate('/posty/comments');
-        console.log('Add Comment clicked');
+        navigate("/posty/comments");
+        console.log("Add Comment clicked");
     };
 
     const handleAddVideo = () => {
         // Handle adding a comment logic here
         // You can implement the functionality to add a comment
         // based on your requirements or use case.
-        navigate('/posty/clips');
-        console.log('Add Video clicked');
+        navigate("/posty/clips");
+        console.log("Add Video clicked");
     };
 
     const handleBackToHomePage = () => {
@@ -28,23 +28,29 @@ function Trausi() {
         // You can implement the functionality to add a comment
         // based on your requirements or use case.
 
-        navigate('/');
-        console.log('Back button clicked');
+        navigate("/");
+        console.log("Back button clicked");
     };
-
 
     return (
         <div className="container">
             <h1>Trausi - Rust Streamer</h1>
-            <p>Welcome to the world of Trausi! Join him on thrilling Rust adventures and witness epic gameplay.</p>
-
+            <p>
+                Welcome to the world of Trausi! Join him on thrilling Rust
+                adventures and witness epic gameplay.
+            </p>
 
             <div className="image-container">
                 <img src={trausiImage} alt="Trausi" />
             </div>
 
             <h2>About Trausi</h2>
-            <p>Trausi is a renowned Rust streamer known for his skilled gameplay and engaging community. He has been streaming Rust for several years and has gained a dedicated following.</p>
+            <p>
+                Trausi is a renowned Rust streamer known for his skilled
+                gameplay and engaging community. He has been streaming Rust for
+                several years and has gained a dedicated following.
+            </p>
+            <p>Game time : 20k hours</p>
 
             <h2>Streaming Schedule</h2>
             <p>Trausi streams Rust on the following schedule:</p>
@@ -55,17 +61,52 @@ function Trausi() {
                 <li>Sunday: 2 PM - 5 PM (EST)</li>
             </ul>
 
-
             <h2>Follow Trausi</h2>
             <p>Make sure to follow Trausi on his streaming platforms:</p>
             <ul>
-                <li>Twitch: <a  className='twitchInfo' href="https://www.twitch.tv/trausi" target="_blank" rel="noopener noreferrer">trausi</a></li>
-                <li>Twitter: <a className='twitterInfo' href="https://twitter.com/trausi" target="_blank" rel="noopener noreferrer">@trausi</a></li>
-                <li>YouTube: <a className='youTubeInfo' href="https://www.youtube.com/trausi" target="_blank" rel="noopener noreferrer">Trausi's YouTube Channel</a></li>
+                <li>
+                    Twitch:{" "}
+                    <a
+                        className="twitchInfo"
+                        href="https://www.twitch.tv/trausi"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        trausi
+                    </a>
+                </li>
+                <li>
+                    Twitter:{" "}
+                    <a
+                        className="twitterInfo"
+                        href="https://twitter.com/trausi"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        @trausi
+                    </a>
+                </li>
+                <li>
+                    YouTube:{" "}
+                    <a
+                        className="youTubeInfo"
+                        href="https://www.youtube.com/trausi"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Trausi's YouTube Channel
+                    </a>
+                </li>
             </ul>
-            <button className='button-comments' onClick={handleAddComment}>Comments</button>
-            <button className='button-video' onClick={handleAddVideo}>Clips </button>
-            <button className='button-video' onClick={handleBackToHomePage}>Back</button>
+            <button className="button-comments" onClick={handleAddComment}>
+                Comments
+            </button>
+            <button className="button-video" onClick={handleAddVideo}>
+                Clips{" "}
+            </button>
+            <button className="button-video" onClick={handleBackToHomePage}>
+                Back
+            </button>
         </div>
     );
 }
