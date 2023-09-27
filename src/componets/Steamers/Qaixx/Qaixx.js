0dd11396b6b1a079edf6cc9a24196a4817ff7307
Daniel Dimitrov <malkothuski@gmail.com>
2023-09-27 15:42:27 +0300

    add time played

--- src/componets/Steamers/Qaixx/Qaixx.js
+++ src/componets/Steamers/Qaixx/Qaixx.js
@@ -1,66 +1,109 @@
-import React from 'react';
-import './Qaixx';
-import qaixxImage from '../../../images/qaixx-image.jpg'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "./Qaixx";
+import qaixxImage from "../../../images/qaixx-image.jpg";
+import { useNavigate } from "react-router-dom";
 
 function Qaixx() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleAddComment = () => {
-    // Handle adding a comment logic here
-    // You can implement the functionality to add a comment
-    // based on your requirements or use case.
+    const handleAddComment = () => {
+        // Handle adding a comment logic here
+        // You can implement the functionality to add a comment
+        // based on your requirements or use case.
 
-    navigate('/posty/comments');
-    console.log('Add Comment clicked');
-  };
+        navigate("/posty/comments");
+        console.log("Add Comment clicked");
+    };
 
-  const handleAddVideo = () => {
-    // Handle adding a comment logic here
-    // You can implement the functionality to add a comment
-    // based on your requirements or use case.
-    navigate('/posty/clips');
-    console.log('Add Video clicked');
+    const handleAddVideo = () => {
+        // Handle adding a comment logic here
+        // You can implement the functionality to add a comment
+        // based on your requirements or use case.
+        navigate("/posty/clips");
+        console.log("Add Video clicked");
+    };
 
-  };
+    const handleBackToHomePage = () => {
+        // Handle adding a comment logic here
+        // You can implement the functionality to add a comment
+        // based on your requirements or use case.
 
-  const handleBackToHomePage = () => {
-    // Handle adding a comment logic here
-    // You can implement the functionality to add a comment
-    // based on your requirements or use case.
+        navigate("/");
+        console.log("Back button clicked");
+    };
 
-    navigate('/');
-    console.log('Back button clicked');
-};
-
-  return (
-    <div className='container'>
-      <h1>Qaixx - Rust Streamer</h1>
-      <p>Join Qaixx on thrilling Rust adventures and witness amazing gameplay in the world of Rust.</p>
-      <div className="image-container">
-        <img src={qaixxImage} alt="Qaixx" />
-      </div>
-      <h2>About Qaixx</h2>
-      <p>Qaixx is a dedicated Rust streamer known for his entertaining streams and engaging community. He has been streaming Rust for several years and enjoys interacting with his viewers.</p>
-      <h2>Streaming Schedule</h2>
-      <p>Qaixx streams Rust on the following schedule:</p>
-      <ul>
-        <li>Monday: 6 PM - 9 PM (EST)</li>
-        <li>Wednesday: 6 PM - 9 PM (EST)</li>
-        <li>Friday: 8 PM - 11 PM (EST)</li>
-      </ul>
-      <h2>Follow Qaixx</h2>
-      <p>Make sure to follow Qaixx on his streaming platforms:</p>
-      <ul>
-        <li>Twitch: <a  className='twitchInfo' href="https://www.twitch.tv/qaixx" target="_blank" rel="noopener noreferrer">qaixx</a></li>
-        <li>Twitter: <a className='twitterInfo' href="https://twitter.com/qaixxxx?lang=bg" target="_blank" rel="noopener noreferrer">@qaixx</a></li>
-        <li>YouTube: <a className='youTubeInfo' href="https://www.youtube.com/qaixx" target="_blank" rel="noopener noreferrer">Qaixx's YouTube Channel</a></li>
-      </ul>
-      <button className='button-comments' onClick={handleAddComment}>Comments</button>
-      <button className='button-video' onClick={handleAddVideo}>Clips </button>
-      <button className='button-video' onClick={handleBackToHomePage}>Back</button>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Qaixx - Rust Streamer</h1>
+            <p>
+                Join Qaixx on thrilling Rust adventures and witness amazing
+                gameplay in the world of Rust.
+            </p>
+            <div className="image-container">
+                <img src={qaixxImage} alt="Qaixx" />
+            </div>
+            <h2>About Qaixx</h2>
+            <p>
+                Qaixx is a dedicated Rust streamer known for his entertaining
+                streams and engaging community. He has been streaming Rust for
+                several years and enjoys interacting with his viewers.
+            </p>
+            <p>Game time: 21k hours</p>
+            <h2>Streaming Schedule</h2>
+            <p>Qaixx streams Rust on the following schedule:</p>
+            <ul>
+                <li>Monday: 6 PM - 9 PM (EST)</li>
+                <li>Wednesday: 6 PM - 9 PM (EST)</li>
+                <li>Friday: 8 PM - 11 PM (EST)</li>
+            </ul>
+            <h2>Follow Qaixx</h2>
+            <p>Make sure to follow Qaixx on his streaming platforms:</p>
+            <ul>
+                <li>
+                    Twitch:{" "}
+                    <a
+                        className="twitchInfo"
+                        href="https://www.twitch.tv/qaixx"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        qaixx
+                    </a>
+                </li>
+                <li>
+                    Twitter:{" "}
+                    <a
+                        className="twitterInfo"
+                        href="https://twitter.com/qaixxxx?lang=bg"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        @qaixx
+                    </a>
+                </li>
+                <li>
+                    YouTube:{" "}
+                    <a
+                        className="youTubeInfo"
+                        href="https://www.youtube.com/qaixx"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Qaixx's YouTube Channel
+                    </a>
+                </li>
+            </ul>
+            <button className="button-comments" onClick={handleAddComment}>
+                Comments
+            </button>
+            <button className="button-video" onClick={handleAddVideo}>
+                Clips{" "}
+            </button>
+            <button className="button-video" onClick={handleBackToHomePage}>
+                Back
+            </button>
+        </div>
+    );
 }
 
 export default Qaixx;
