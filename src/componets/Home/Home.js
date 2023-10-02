1c23184bee8d566cc12cfcbdced0a337e16f9551
Daniel Dimitrov <malkothuski@gmail.com>
2023-10-02 16:36:09 +0300

    css errors

--- src/componets/Home/Home.js
+++ src/componets/Home/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../Home/Home.css";
 import { Link, useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Home() {
     const history = useNavigate();
@@ -22,26 +21,26 @@ function Home() {
 
     return (
         <body>
-            <div className="Container">
-                <section>
-                    <div className="heading-1">
-                        <h2>
-                            Welcome to the world of Rust{" "}
-                            {isLoggedIn && <h2>{name}</h2>}
-                            {isLoggedIn && (
-                                <button
-                                    className="button_logout"
-                                    onClick={handleLogout}
-                                >
-                                    Logout
-                                </button>
-                            )}
-                        </h2>
-                        <FontAwesomeIcon icon="fa-solid fa-gun" />
-                    </div>
-                </section>
+            <div className="Home-main">
+                <div className="Container">
+                    <section>
+                        <div className="heading-1">
+                            <h2>
+                                Welcome to the world of Rust{" "}
+                                {isLoggedIn && <h2>{name}</h2>}
+                                {isLoggedIn && (
+                                    <button
+                                        className="button_logout"
+                                        onClick={handleLogout}
+                                    >
+                                        Logout
+                                    </button>
+                                )}
+                            </h2>
+                        </div>
+                    </section>
 
-                {/*    <head>
+                    {/*    <head>
                     <meta charset="UTF-8" />
                     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -53,118 +52,119 @@ function Home() {
                     <title>Animate</title>
                 </head> */}
 
-                <section>
-                    <div className="home-container">
-                        {/*  <div className="row-item item-1"></div> */}
-                        <div className="item item-2">
-                            <div className="posty-rust"></div>
-                            <Link
-                                to="/posty"
-                                className="posty-image"
-                                onClick={onClickHandler}
-                            >
-                                Posty
-                            </Link>
-                        </div>
-                        <div className="item item-3">
-                            <div className="qaixx-rust"></div>
-                            <Link
-                                to="/qaixx"
-                                className="qaixx-image"
-                                onClick={onClickHandler}
-                            >
-                                Qaixx
-                            </Link>
-                        </div>
-                        <div className="item item-4">
-                            <div className="albin-rust"></div>
-                            <Link
-                                to="/albin"
-                                className="albin-image"
-                                onClick={onClickHandler}
-                            >
-                                Albin
-                            </Link>
-                        </div>
-                        <div className="item item-5">
-                            <div className="trausi -rust"></div>
-                            <Link
-                                to="/trausi"
-                                className="trausi-image"
-                                onClick={onClickHandler}
-                            >
-                                Trausi{" "}
-                            </Link>
-                        </div>
-                        <div className="item item-6">
-                            <div className="deathwingua-rust"></div>
-                            <Link
-                                to="/deathwingua "
-                                className="deathwingua-image"
-                                onClick={onClickHandler}
-                            >
-                                Deathwingua
-                            </Link>
-                        </div>
-                        <div className="item item-7">
-                            <div className="hJune-rust"></div>
-                            <Link
-                                to="/hJune "
-                                className="hJune-image"
-                                onClick={onClickHandler}
-                            >
-                                hJune
-                            </Link>
-                        </div>
-                        <div className="item item-8">
-                            <div className="r00t9r-rust"></div>
-                            <Link
-                                to="/r00t9r "
-                                className="r00t9r-image"
-                                onClick={onClickHandler}
-                            >
-                                r00t9r
-                            </Link>
-                        </div>
-                        <div className="item item-9">
-                            <div className="CoconutB-rust"></div>
-                            <Link
-                                to="/CoconutB "
-                                className="coco-image"
-                                onClick={onClickHandler}
-                            >
-                                CoconutB
-                            </Link>
-                        </div>
-                        <div className="item item-10">
-                            <div className="Riqqeloff-rust"></div>
-                            <Link
-                                to="/Riqqeloff "
-                                className="riqqeloff-image"
-                                onClick={onClickHandler}
-                            >
-                                Riqqeloff
-                            </Link>
-                        </div>
-                        <div className="item item-11">
-                            <div className="Dyanna-rust"></div>
-                            <Link
-                                to="/Dyanna "
-                                className="Dyanna-image"
-                                onClick={onClickHandler}
-                            >
-                                Dyanna
-                            </Link>
-                        </div>
+                    <section>
+                        <div className="home-container">
+                            {/*  <div className="row-item item-1"></div> */}
+                            <div className="item item-2">
+                                <div className="posty-rust"></div>
+                                <Link
+                                    to="/posty"
+                                    className="posty-image"
+                                    onClick={onClickHandler}
+                                >
+                                    Posty
+                                </Link>
+                            </div>
+                            <div className="item item-3">
+                                <div className="qaixx-rust"></div>
+                                <Link
+                                    to="/qaixx"
+                                    className="qaixx-image"
+                                    onClick={onClickHandler}
+                                >
+                                    Qaixx
+                                </Link>
+                            </div>
+                            <div className="item item-4">
+                                <div className="albin-rust"></div>
+                                <Link
+                                    to="/albin"
+                                    className="albin-image"
+                                    onClick={onClickHandler}
+                                >
+                                    Albin
+                                </Link>
+                            </div>
+                            <div className="item item-5">
+                                <div className="trausi -rust"></div>
+                                <Link
+                                    to="/trausi"
+                                    className="trausi-image"
+                                    onClick={onClickHandler}
+                                >
+                                    Trausi{" "}
+                                </Link>
+                            </div>
+                            <div className="item item-6">
+                                <div className="deathwingua-rust"></div>
+                                <Link
+                                    to="/deathwingua "
+                                    className="deathwingua-image"
+                                    onClick={onClickHandler}
+                                >
+                                    Deathwingua
+                                </Link>
+                            </div>
+                            <div className="item item-7">
+                                <div className="hJune-rust"></div>
+                                <Link
+                                    to="/hJune "
+                                    className="hJune-image"
+                                    onClick={onClickHandler}
+                                >
+                                    hJune
+                                </Link>
+                            </div>
+                            <div className="item item-8">
+                                <div className="r00t9r-rust"></div>
+                                <Link
+                                    to="/r00t9r "
+                                    className="r00t9r-image"
+                                    onClick={onClickHandler}
+                                >
+                                    r00t9r
+                                </Link>
+                            </div>
+                            <div className="item item-9">
+                                <div className="CoconutB-rust"></div>
+                                <Link
+                                    to="/CoconutB "
+                                    className="coco-image"
+                                    onClick={onClickHandler}
+                                >
+                                    CoconutB
+                                </Link>
+                            </div>
+                            <div className="item item-10">
+                                <div className="Riqqeloff-rust"></div>
+                                <Link
+                                    to="/Riqqeloff "
+                                    className="riqqeloff-image"
+                                    onClick={onClickHandler}
+                                >
+                                    Riqqeloff
+                                </Link>
+                            </div>
+                            <div className="item item-11">
+                                <div className="Dyanna-rust"></div>
+                                <Link
+                                    to="/Dyanna "
+                                    className="Dyanna-image"
+                                    onClick={onClickHandler}
+                                >
+                                    Dyanna
+                                </Link>
+                            </div>
 
-                        {/* <div className="row-item item-6"></div> */}
-                    </div>
-                </section>
+                            {/* <div className="row-item item-6"></div> */}
+                        </div>
+                    </section>
 
-                {/*  <div className="btn-more">
+                    {/*  <div className="btn-more">
                     <Link to='/learnmore' className="learn-more" onClick={onClickHandler}>learn more </Link>
                       <button className="btn-more" onClick={learnMore}>learn more</button> 
                 </div>  */}
+                </div>
             </div>
         </body>
     );
